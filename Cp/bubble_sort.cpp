13b362ac4f097b6f8d60f739f20aa9920da47ba9
sanp9 <patel.san360@gmail.com>
2020-11-15 22:24:27 +0530

    bubble_sortSelection_sort

---
+++ Cp/bubble_sort.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void bubble_sort(int* arr, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			if (arr[j] < arr[i])
+				swap(arr[i], arr[j]);
+		}
+	}
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	int* arr = new int[n];
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+
+	bubble_sort(arr, n);
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
+}
